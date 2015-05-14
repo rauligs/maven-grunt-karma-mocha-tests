ad3b3e16d61117b20c2a823fed900bd457c47b03
Raul Suarez <suarezr@visa.com>
2015-05-14 12:04:09 +0100

    Removed crap added to check Istanbul coverage report

--- src/main/js/calculator.js
+++ src/main/js/calculator.js
@@ -19,14 +19,3 @@ Calculator.prototype.add = function() {
         return a + b;
     });
 };
-
-Calculator.prototype.whatever = function() {
-    var values = this._filter([].slice.apply(arguments));
-
-    if (values.length < 2) {
-        throw new Error('Add expects a minimum of 2 arguments but ' + values.length + ' given.');
-    }
-    return values.reduce(function(a, b) {
-        return a + b;
-    });
-};
